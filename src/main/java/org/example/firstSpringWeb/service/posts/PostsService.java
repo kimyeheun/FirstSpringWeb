5e986a118784b18f05cbd2d0877a42ae6da54f2e
김예은 <yeheun004@gmail.com>
2022-05-06 00:18:22 +0900

    등록/수정/조회 API 만들기 (2)

--- src/main/java/org/example/firstSpringWeb/service/posts/PostsService.java
+++ src/main/java/org/example/firstSpringWeb/service/posts/PostsService.java
@@ -3,8 +3,9 @@ package org.example.firstSpringWeb.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.firstSpringWeb.domain.posts.Posts;
 import org.example.firstSpringWeb.domain.posts.PostsRepository;
-import org.example.firstSpringWeb.web.dto.PostsResponsDto;
+import org.example.firstSpringWeb.web.dto.PostsResponseDto;
 import org.example.firstSpringWeb.web.dto.PostsSaveRequestDto;
+import org.example.firstSpringWeb.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
@@ -19,16 +20,16 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id, PostsResponsDto.PostsUpdateRequestDto requestDto) {
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponsDto findById(Long id) {
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
-        return new PostsResponsDto(entity);
+        return new PostsResponseDto(entity);
     }
 
 }
